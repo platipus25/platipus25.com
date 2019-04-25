6aa4cb67e7f109f7a6e273d7f4f284f50d886e4b
platipus25 <40722163+platipus25@users.noreply.github.com>
2019-04-24 17:15:36 -0700

    add icons

--- components/head.js
+++ components/head.js
@@ -15,10 +15,10 @@ const Head = props => (
       content={props.description || defaultDescription}
     />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" sizes="192x192" href="/static/touch-icon.png" />
-    <link rel="apple-touch-icon" href="/static/touch-icon.png" />
-    <link rel="mask-icon" href="/static/favicon-mask.svg" color="#49B882" />
-    <link rel="icon" href="/static/favicon.ico" />
+    <link rel="icon" sizes="192x192" href="/static/icons/touch-icon.png" />
+    <link rel="apple-touch-icon" href="/static/icons/touch-icon.png" />
+    <link rel="mask-icon" href="/static/icons/favicon-mask.svg" color="#49B882" />
+    <link rel="icon" href="/static/icons/favicon.ico" />
     <meta property="og:url" content={props.url || defaultOGURL} />
     <meta property="og:title" content={props.title || ''} />
     <meta
@@ -31,6 +31,22 @@ const Head = props => (
     <meta property="og:image" content={props.ogImage || defaultOGImage} />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
+
+    <link rel="apple-touch-icon" sizes="57x57" href="/static/icons/apple-icon-57x57.png" />
+    <link rel="apple-touch-icon" sizes="60x60" href="/static/icons/apple-icon-60x60.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="/static/icons/apple-icon-72x72.png" />
+    <link rel="apple-touch-icon" sizes="76x76" href="/static/icons/apple-icon-76x76.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="/static/icons/apple-icon-114x114.png" />
+    <link rel="apple-touch-icon" sizes="120x120" href="/static/icons/apple-icon-120x120.png" />
+    <link rel="apple-touch-icon" sizes="144x144" href="/static/icons/apple-icon-144x144.png" />
+    <link rel="apple-touch-icon" sizes="152x152" href="/static/icons/apple-icon-152x152.png" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/static/icons/apple-icon-180x180.png" />
+    <link rel="icon" type="image/png" sizes="192x192"  href="/static/icons/android-icon-192x192.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/static/icons/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="96x96" href="/static/icons/favicon-96x96.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/static/icons/favicon-16x16.png" />
+    <link rel="manifest" href="/static/icons/manifest.json" />
+    <meta name="theme-color" content="#ffffff" />
   </NextHead>
 )
 
