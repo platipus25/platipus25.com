e2cc9116e2c85d1dde2cd4e31dfa88be8443f3c0
platipus25 <40722163+platipus25@users.noreply.github.com>
2019-04-28 16:40:23 -0700

    seperate out APIDoc

--- pages/whatsnext/api.js
+++ pages/whatsnext/api.js
@@ -5,7 +5,7 @@ import superagent from 'superagent'
 class APIDoc extends React.Component {
     constructor(props){
         super(props)
-        this.state = { apiCode: "g" }
+        this.state = { apiCode: "" }
         superagent.get('https://whatsnext.cdn.platipus25.com/whatsnext.d.ts')
         .then((data) => {
             let apiCode = data.text
@@ -15,15 +15,11 @@ class APIDoc extends React.Component {
 
     render(){
         return (
-        <div className="body">
-            <Head title="API Docs"></Head>
+        <div className="apiDoc">
             <p></p>
             <code id="apiCode"><pre>{this.state.apiCode}</pre></code>
             <style jsx>{`
-                .body{
-                    width:100%;
-                    height:100vh;
-                }
+                
             `}</style>
         </div>
         )
@@ -31,8 +27,34 @@ class APIDoc extends React.Component {
 }
 
 export default () => (
-    <div>
-
-    <APIDoc></APIDoc>
+    <div className="body">
+        <Head title="API Docs"/>
+        <h1 className="title">API Documentation</h1>
+        <APIDoc className="content"></APIDoc>
+        <style jsx>{`
+                .body{
+                    margin: 1em;
+                    font-family: sans-serif;
+                    height:100vh;
+                }
+    
+                .title{
+                    text-align: center;
+                    font-family: "Courier", "Courier New", sans-serif;
+                    margin:0px;
+                }
+    
+                h1.title{
+                    padding-top:6vh;
+                }
+    
+                .content{
+                    padding: 3em;
+                }
+    
+                .content p{
+                    font-size: 1.5em;
+                }
+            `}</style>
     </div>
 )
